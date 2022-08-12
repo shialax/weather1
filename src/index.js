ebd1bfbe7aad72de5d77ed398126b2fe178f0fb1
shialax <74329916+shialax@users.noreply.github.com>
2022-08-12 10:06:39 -0400

    removed event

--- src/index.js
+++ src/index.js
@@ -76,9 +76,6 @@ dateElement.innerHTML = formatDate(currentTime);
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleSubmit);
 
-let searchInput = document.querySelector("search-input");
-searchInput.addEventListener("submit", handleSubmit);
-
 let currentLocationButton = document.querySelector("#current-location-button");
 currentLocationButton.addEventListener("click", getCurrentLocation);
 
